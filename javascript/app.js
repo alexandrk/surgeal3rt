d7facc218ba33f72d410b9a2ac6044526502a10e
Alexander Kazakov <alexander@kazakov.mobi>
2015-12-18 14:34:59 -0800

    Reorganizes code in a more readable manner Adds uriAnchor functionality for 'service type' and 'limit' for the initial number of records retreived Adds ability to switch between different 'service types' on the fly, by use of buttons

--- javascript/app.js
+++ javascript/app.js
@@ -1,73 +1,159 @@
-var
-  data = [],
-  lastElement,
-  queryDict = {},
-  validTypes = ['uberX', 'uberXL', 'uberWAV', 'uberSELECT', 'UberSUV', 'UberBLACK', 'ASSIST'];
+var app = function() {
 
-// Helper, populates queryDict with key-value pairs
-location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]});
-
-function loadData() {
   var
-    ref = new Firebase("https://surgeal3rt.firebaseio.com/uber_surge_data"),
-    limit = ( queryDict['limit'] && parseInt(queryDict['limit']) > 0 ) ? parseInt(queryDict['limit']) : 25;
+    DATA = [],
+    ANCHOR_MAP    = $.uriAnchor.makeAnchorMap();
 
-  ref.orderByChild('time').limitToLast(limit).on("child_added", function(snapshot) {
-    lastElement = snapshot.val();
-    data.push(lastElement);
-    $(document).trigger('dataLoaded');
-  });
-}
+  /**
+   * Name:        loadData
+   * Description: loads data from the firebase database, triggering
+   *              dataloaded event on each new set of data
+   */
+  function loadData () {
+    var
+      ref   = new Firebase("https://surgeal3rt.firebaseio.com/uber_surge_data"),
+      limit = ( parseInt(ANCHOR_MAP['limit']) > 0 ) ? parseInt(ANCHOR_MAP['limit']) : 25,
+      dataset;
 
-function getServiceTypeSurge(surgeData, serviceType) {
-  var results = [];
+    ref.orderByChild('time').limitToLast(limit).on("child_added", function(snapshot)
+    {
+      // Save Global lastElement for later use
+      dataset = snapshot.val();
 
-  for ( loc in surgeData ) {
-    results[loc] = surgeData[loc][serviceType];
+      // Push each newly received element into Global data array
+      DATA.push(dataset);
+
+      processData(dataset);
+    });
   }
 
-  return results;
-}
+  /**
+   * Name:          processData
+   * Description:   Processes a single chunk of data (either from a data array or
+   *                as a result of a new set coming from firebase)
+   */
+  function processData(dataset) {
+    var
+      serviceType = validateServiceType(ANCHOR_MAP['type']),
+      surgeData;
 
-function formatAndOutput( time, values, serviceType ) {
-  var
-    newSet = $('<div class="aSet">'),
-    timeObj = ( new Date(parseInt( time ) * 1000) ),
-    time = timeObj.toLocaleTimeString() +' '+ timeObj.toLocaleDateString();
+    surgeData = getServiceTypeSurge(dataset.data, serviceType);
+    formatAndOutput(dataset.time, surgeData, serviceType);
+  }
 
-  newSet.hide();
+  /**
+   * Name:          validateServiceType
+   * Description:   Checks if provided service type matches one of the service types specified by Uber API
+   *                Sets Global SERVICE_TYPE variable to the proper case of the matched value
+   * @param serviceType   - {string} service type in question
+   * @returns {string}    - serviceType from the list of types specified by Uber API
+   */
+  function validateServiceType( serviceType ) {
+    var
+      validTypes = ['uberX', 'uberXL', 'uberWAV', 'uberSELECT', 'UberSUV', 'UberBLACK', 'ASSIST'],
+      index = -1,
+      matchFound;
 
-  // iterate over neighbourhood in a set to display the surge
-  for (val in values) {
-    newSet.append(
-      "<div class='row'>" +
-        "<div class='neighbourhood'>" + val + "</div>" +
-        "<div class='surge'>" + values[val] + "</div>" +
+    // Lowercase the type in question
+    serviceType = ( typeof serviceType === 'string' ) ? serviceType.toLowerCase() : '';
+
+    // Case insensitive match of the provided value vs values approved by Uber API
+    matchFound = validTypes.some( function( element, i ) {
+      if ( element.toLowerCase() === serviceType ) {
+        index = i;
+        serviceType = validTypes[index];
+        return true;
+      }
+    });
+    return ( matchFound ) ? serviceType : validTypes[0];
+  }
+
+  /**
+   * Name:              getServiceTypeSurge()
+   * Description:       Creates an array of objects, that represent surge in a specific area
+   * @param surgeData   {object} - contains surge data for all of the service_types
+   * @param serviceType {string} - serviceType to get the data for
+   * @returns {Array}
+   */
+  function getServiceTypeSurge( surgeData, serviceType ) {
+    var results = [];
+
+    for ( loc in surgeData ) {
+      results[loc] = surgeData[loc][serviceType];
+    }
+
+    return results;
+  }
+
+  /**
+   * Name:              formatAndOutput
+   * Description:       formats the results and appends them to the page
+   * @param time        {unixtimestamp} - time, when the set was requested from Uber API
+   * @param values      {array} of surgeData
+   * @param serviceType {string} - service type of the current set
+   */
+  function formatAndOutput( time, values, serviceType ) {
+    var
+      newSet = $('<div class="aSet">'),
+      timeObj = ( new Date(parseInt( time ) * 1000) ),
+      time = timeObj.toLocaleTimeString() +' '+ timeObj.toLocaleDateString();
+
+    newSet.hide();
+
+    // iterate over neighbourhood in a set to display the surge
+    for (val in values) {
+      newSet.append(
+        "<div class='row'>" +
+          "<div class='neighbourhood'>" + val + "</div>" +
+          "<div class='surge'>" + values[val] + "</div>" +
+        "</div>"
+      );
+    }
+
+    newSet.prepend(
+      "<div class='row header'>" +
+      "<div class='serviceType'>" + serviceType + "</div>" +
+      "<div class='time'>" + time + "</div>" +
       "</div>"
     );
+
+    //$('.data').prepend(newSet);
+    newSet.prependTo('.data').show('slow');
   }
 
-  newSet.prepend(
-    "<div class='row header'>" +
-    "<div class='serviceType'>" + serviceType + "</div>" +
-    "<div class='time'>" + time + "</div>" +
-    "</div>"
-  );
+  function onHashchange( event ) {
 
-  //$('.data').prepend(newSet);
-  newSet.prependTo('.data').show('slow');
-}
+    var oldAnchorMap = ANCHOR_MAP;
+    ANCHOR_MAP = $.uriAnchor.makeAnchorMap();
 
-$(document).on('dataLoaded', function(){
-  var
-    serviceType = ( (validTypes.indexOf(queryDict['type']) > 0) ? queryDict['type'] : 'uberX' ),
-    surgeData;
+    // Clear the display
+    $('.data').empty();
 
-  surgeData = getServiceTypeSurge(lastElement.data, serviceType);
+    // Populate the display with the data for a newly selected serviceType
+    DATA.forEach(function(el) {
+      processData(el);
+    });
+  }
 
-  formatAndOutput(lastElement.time, surgeData, serviceType);
-});
+  function onTypeChange( event ) {
+    var
+      newType = event.target.firstChild.data;
 
-loadData();
+    anchorMap = {type: newType};
+    $.uriAnchor.setAnchor( anchorMap );
 
+  }
+
+  $(window)
+    .bind( 'hashchange', onHashchange );
+
+  $('.controls').find('button').bind( 'click', onTypeChange );
+
+  return {
+    run: loadData
+  }
+
+}();
+
+app.run();
 //K5h7xwrgQgoq-Kk54l5 firebase key where new data formatting starts

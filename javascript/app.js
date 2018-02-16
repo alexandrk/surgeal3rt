73cadf19d8d6247ebe9c8941bf50eb67748e4ee2
Alexander Kazakov <alexander@kazakov.mobi>
2018-02-16 14:14:10 -0800

    Switched to new database

--- javascript/app.js
+++ javascript/app.js
@@ -12,7 +12,8 @@ var app = function() {
   function loadData () {
     var
       //ref   = new Firebase("https://surgeal3rt.firebaseio.com/uber_surge_data"),
-        ref   = new Firebase("https://surgeal3rtp2.firebaseio.com/uber_surge_data"),
+      //ref   = new Firebase("https://surgeal3rtp2.firebaseio.com/uber_surge_data"),
+      ref   = new Firebase("https://surgeal3rtp3.firebaseio.com/uber_surge_data"),  // Since 02/16/2018 2:00pm
       limit = ( parseInt(ANCHOR_MAP['limit']) > 0 ) ? parseInt(ANCHOR_MAP['limit']) : 25,
       dataset;
 
--- javascript/firebase_interactions.js
+++ javascript/firebase_interactions.js
@@ -17,7 +17,8 @@ var FI = (function firebase_interactions(){
 
     // NOTE: timestamp saved in the firebase db is in seconds, so we need to divide ours by 1000
     //var ref = new Firebase("https://surgeal3rt.firebaseio.com/uber_surge_data");
-    var ref = new Firebase("https://surgeal3rtp2.firebaseio.com/uber_surge_data");    
+    //var ref = new Firebase("https://surgeal3rtp2.firebaseio.com/uber_surge_data");  
+    var ref = new Firebase("https://surgeal3rtp3.firebaseio.com/uber_surge_data");  // Since 02/16/2018 2:00pm
 
     // Setting fromTime to beginning of current day, if undefined
     if (configObj.fromTime === undefined ){

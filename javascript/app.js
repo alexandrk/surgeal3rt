96d5cfc0c2201240b9d80a81183835372dbc4331
Alexander Kazakov <alexander@kazakov.mobi>
2018-02-16 14:23:39 -0800

    Moved to a new datastore

--- javascript/app.js
+++ javascript/app.js
@@ -13,6 +13,7 @@ var app = function() {
     var
       //ref   = new Firebase("https://surgeal3rt.firebaseio.com/uber_surge_data"),
         ref   = new Firebase("https://surgeal3rtp2.firebaseio.com/uber_surge_data"),
+        ref   = new Firebase("https://surgeal3rtp3.firebaseio.com/uber_surge_data"),
       limit = ( parseInt(ANCHOR_MAP['limit']) > 0 ) ? parseInt(ANCHOR_MAP['limit']) : 25,
       dataset;
 
